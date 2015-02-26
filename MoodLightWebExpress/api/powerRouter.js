8f2492ab898549729ccbb0024138f7fb7a7d037a
bartdewallef <bartdewallef@gmail.com>
2015-02-26 19:19:17 +0100

    Power toggle functionality. > Communicates with backend

---
+++ MoodLightWebExpress/api/powerRouter.js
@@ -0,0 +1,14 @@
+﻿var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.json(req.app.locals.power);
+})
+
+router.post('/', function (req, res) {
+    console.log(req.body)
+    req.app.locals.power = req.body;
+    res.send(req.body);
+})
+
+module.exports = router;
--- MoodLightWebExpress/public/javascripts/site.js
+++ MoodLightWebExpress/public/javascripts/site.js
@@ -1,4 +1,44 @@
-﻿$("[name='power-toggle']").bootstrapSwitch({
+﻿var powerState = false;
+var powerDisabled = true;
+
+var powerToggle = $("[name='power-toggle']").bootstrapSwitch({
     size: 'small',
-    wrapperClass: 'power-toggle'
+    wrapperClass: 'power-toggle',
+    state: powerState,
+    disabled: powerDisabled
 });
+
+function updatePowerToggle(data) {
+    powerState = data.power;
+    powerDisabled = false;
+    powerToggle.bootstrapSwitch('disabled', powerDisabled);
+    powerToggle.bootstrapSwitch('state', powerState);
+}
+
+// Get state of power
+$.ajax({
+    url: "/power",
+}).done(function (data) {
+    console.debug("Current power state: " + data.power);
+    updatePowerToggle(data); 
+});
+
+
+
+$("[name='power-toggle']").on('switchChange.bootstrapSwitch', function (event, state) {
+    console.debug("State change: " + state);
+    powerState = state;
+    powerDisabled = true;
+    powerToggle.bootstrapSwitch('disabled', powerDisabled);
+    $.ajax({
+        url: "/power",
+        type: "POST",
+        data: JSON.stringify({ power: powerState }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+    }).success(function (data){
+        updatePowerToggle(data);
+    })
+});
+
+
